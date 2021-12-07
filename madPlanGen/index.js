78b8cabb126b68f8da6b121080203067b027e80a
Tr4shL0rd <ketil04@gmail.com>
2021-12-07 14:00:21 +0100

    added a button and prettified things

--- madPlanGen/index.js
+++ madPlanGen/index.js
@@ -1,20 +1,31 @@
-// const foods = ["mad1", "mad2", "mad3", "mad4", "mad5", "mad6"]
-const foods = ["oksekød med ris", "pizza med stuff", "speghetti kødsovs", "hestemad", "rundstykker", "kødsovs med ris"]
-const days  = ["Mandag: ", "Tirsdag:", "Onsdag: ", "Torsdag:", "Fredag: ", "Lørdag: "]
+let debug = true;
+function generatePlan() {
+    // const foods = ["mad1", "mad2", "mad3", "mad4", "mad5", "mad6"]
+    const foods = ["oksekød med ris", "pizza med stuff", "speghetti kødsovs", "hestemad", "rundstykker", "kødsovs med ris"]
+    const days  = ["Mandag: ", "Tirsdag:", "Onsdag: ", "Torsdag:", "Fredag: ", "Lørdag: "]
 
-let foodTable = "<tr>";
-let dayText = "dag";
-// fills the table with random food to eat for each day
-// for (let _ of days) {                       // Cant decide which one to use
-for (let day = 0; day < days.length; day++) {  // Cant decide which one to use
-    
-    // picks a random element from foods array
-    rng = Math.floor(Math.random() * foods.length); 
-    // adds the random element to the table
-    foodTable += `<td>${foods[rng]}</td>`; 
+    let foodTable = "<tr>";
+    let dayText = "dag";
+    // fills the table with random food to eat for each day
+    // for (let _ of days) {                       // Cant decide which one to use
+    for (let day = 0; day < days.length; day++) {  // Cant decide which one to use
+        // implement day text
+        // picks a random element from foods array
+        rng = Math.floor(Math.random() * foods.length); 
+        // adds the random element to the table
+        foodTable += `<td id='foodCell'>${foods[rng]}</td><tr>`; 
 
+    }
+    foodTable += "</tr></table>";
+    document.getElementById("foodPlacement").innerHTML = foodTable;
 }
 
 
-foodTable += "</tr></table>";
-document.getElementById("foodPlacement").innerHTML = foodTable;
+
+
+
+
+
+if (debug) {
+    generatePlan();
+}
