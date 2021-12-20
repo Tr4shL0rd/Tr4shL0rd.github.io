7395aece62c8c849e6c2012d9f2eb01758de1f8c
Tr4shL0rd <ketil04@gmail.com>
2021-12-20 17:43:49 +0100

    commit

--- madPlanGen/index.js
+++ madPlanGen/index.js
@@ -1,4 +1,9 @@
 let debug = true;
+function removeDup(Arr) {
+    let uniqArr = [...new Set(Arr)];
+    return uniqArr
+}
+
 function generatePlan() {
     // const foods = ["mad1", "mad2", "mad3", "mad4", "mad5", "mad6"]
     const foods = ["oksekød med ris", "pizza med stuff", "speghetti kødsovs", "hestemad", "rundstykker", "kødsovs med ris"]
@@ -6,15 +11,33 @@ function generatePlan() {
 
     let foodTable = "<tr>";
     let dayText = "dag";
+    var foodArray = []
+    
+    
     // fills the table with random food to eat for each day
-    // for (let _ of days) {                       // Cant decide which one to use
-    for (let day = 0; day < days.length; day++) {  // Cant decide which one to use
-        // implement day text
-        // picks a random element from foods array
+    for (let day = 0; day < days.length; day++) {  
         rng = Math.floor(Math.random() * foods.length); 
+        var foodForDay = foods[rng]
+        foodArray.push(foodForDay);
+        // picks a random element from foods array
+        if (removeDup(foodArray).length != 6){
+            console.log("too short!");
+        }
         // adds the random element to the table
-        foodTable += `<td id='foodCell'>${foods[rng]}</td><tr>`; 
-
+        console.log(foodArray)
+        if (foodArray.length == 6 && removeDup(foodArray).length == 6) {
+            console.log(foodArray[day]);
+        }
+        foodTable += `<td id='foodCell'>${foodArray[day]}</td><tr>`; 
+        
+    }
+    
+    // console.log(foodArray.length);
+    let uniqFoods = removeDup(foodArray);
+    // console.log(uniqFoods);
+    if (foodArray.length != uniqFoods.length) {
+        // console.error("FUCK");
+        generatePlan();
     }
     foodTable += "</tr></table>";
     document.getElementById("foodPlacement").innerHTML = foodTable;
--- madPlanGen/test_script.js
+++ madPlanGen/test_script.js
@@ -1,20 +1,6 @@
-// const foods = ["mad1", "mad2", "mad3", "mad4", "mad5", "mad6"]
-const foods = ["oksekød med ris", "pizza med stuff", "speghetti kødsovs", "hestemad", "rundstykker", "kødsovs med ris"]
-const days  = ["Mandag: ", "Tirsdag:", "Onsdag: ", "Torsdag:", "Fredag: ", "Lørdag: "]
-
-let foodTable = "<tr>";
-let dayText = "dag";
-// fills the table with random food to eat for each day
-// for (let _ of days) {                       // Cant decide which one to use
-for (let day = 0; day < days.length; day++) {  // Cant decide which one to use
-    
-    // picks a random element from foods array
-    rng = Math.floor(Math.random() * foods.length); 
-    // adds the random element to the table
-    foodTable += `<td>${foods[rng]}</td>`; 
+function removeDup(Arr) {
+    let uniqArr = [...new Set(Arr)];
+    return uniqArr
 
 }
 
-
-foodTable += "</tr></table>";
-document.getElementById("foodPlacement").innerHTML = foodTable;
